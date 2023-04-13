ee860b13df518903c50a3b55cecae6c4c8ac2d56
gjy <3115354059@qq.com>
2023-04-13 12:38:42 +0800

    1

---
+++ src/main/java/com/example/friendss/FriendSsApplication.java
@@ -0,0 +1,13 @@
+package com.example.friendss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendSsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendSsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/friendss/FriendSsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.friendss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendSsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
